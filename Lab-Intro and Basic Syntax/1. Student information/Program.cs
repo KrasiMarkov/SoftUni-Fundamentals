a7a343bdda4310c611bc19a86694e94bf1335726
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-20 16:04:42 +0300

    Add files via upload

---
+++ Lab-Intro and Basic Syntax/1. Student information/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1._Student_information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {averageGrade:F2}");
+        }
+    }
+}
