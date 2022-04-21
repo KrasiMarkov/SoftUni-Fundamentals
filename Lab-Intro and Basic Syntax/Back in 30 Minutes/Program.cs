6bab0aa6dbec5d60392f723e637a42bae2da2d06
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-21 23:25:18 +0300

    Add files via upload

---
+++ Lab-Intro and Basic Syntax/Back in 30 Minutes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Back_in_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+
+
+
+            if (minutes < 30)
+            {
+                minutes = minutes + 30;
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+            else
+            {
+                minutes =  60 - minutes;
+                hours = hours + 1;
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+
+            
+        }
+    }
+}
