23bf2263eaddf56ece49ea8b2aa5c08dda690771
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-22 23:30:32 +0300

    Add files via upload

---
+++ Lab-Intro and Basic Syntax/Foreign Languages/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameCountry = Console.ReadLine();
+            if (nameCountry == "USA" || nameCountry == "England")
+            {
+                Console.WriteLine("English");
+            }
+            else if (nameCountry == "Spain" || nameCountry == "Argentina" || nameCountry == "Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
