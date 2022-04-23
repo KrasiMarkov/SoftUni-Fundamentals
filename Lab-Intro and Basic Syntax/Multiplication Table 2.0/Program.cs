17e1f6a453cede116479f9ce5f256a26a3a9fa05
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-23 22:16:29 +0300

    Add files via upload

---
+++ Lab-Intro and Basic Syntax/Multiplication Table 2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int j = int.Parse(Console.ReadLine());
+            if (n <= 10 && j <= 10)
+            {
+                for (int i = j; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+                }
+            }
+            else if (n > 10 || j > 10 )
+            {
+                Console.WriteLine($"{n} X {j} = {n * j}");
+            }
+                
+        }
+    }
+}
