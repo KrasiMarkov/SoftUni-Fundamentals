4cb776d8f576341cb94bbca57b6308a7037d8cdf
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-02 22:41:29 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/Login/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = "";
+            
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password = password + username[i];
+            }
+            string pass = Console.ReadLine();
+            int counter = 0;
+            while (pass != password)
+            {
+                counter++;
+                if (counter == 4)
+                {
+                    
+                    break;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+
+                pass = Console.ReadLine();
+            }
+            if (pass == password)
+            {
+
+                Console.WriteLine($"User {username} logged in.");
+            }
+            else
+            {
+                Console.WriteLine($"User {username} blocked!");
+            }
+        }
+    }
+}
