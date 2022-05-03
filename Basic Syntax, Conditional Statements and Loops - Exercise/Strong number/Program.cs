0aa3d5a3d3496b7da3efff0c6ae31075761ba6db
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-03 22:48:30 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/Strong number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            int tempNum = number;
+            int sum = 0;
+            while (tempNum > 0)
+            {
+                int digit = tempNum % 10;
+                tempNum = tempNum / 10;
+                int result = 1;
+                for (int i = 1; i <= digit; i++)
+                {
+                    result = result * i;
+                    
+                }
+                sum = sum + result;
+            }
+            if (sum == number)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+
+        }
+    }
+}
