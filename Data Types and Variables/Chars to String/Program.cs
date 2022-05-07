6b261db360890efdca3591bd4ff9e8546ce72e6e
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-07 17:41:16 +0300

    Add files via upload

---
+++ Data Types and Variables/Chars to String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sumOfSymbols = "";
+            for (int i = 1; i <= 3; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                sumOfSymbols = sumOfSymbols + symbol;
+
+            }
+            Console.WriteLine(sumOfSymbols);
+        }
+    }
+}
