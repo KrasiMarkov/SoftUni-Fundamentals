3821150867a80ab9323113174eebb05f16a715a8
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-08 23:12:47 +0300

    Add files via upload

---
+++ Data Types and Variables/Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float meters = float.Parse(Console.ReadLine());
+            float kilometers = 0;
+            kilometers = meters * 0.001f;
+            Console.WriteLine($"{kilometers:F2}");
+        }
+    }
+}
