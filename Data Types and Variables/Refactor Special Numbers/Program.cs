16e0dd2c79e1e066dc70dcba27474ae58cc30c3a
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-09 23:49:16 +0300

    Add files via upload

---
+++ Data Types and Variables/Refactor Special Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= number; i++)
+            {
+                int digit = i;
+
+                int sumOfDigits = 0;
+
+                while (digit > 0)
+                {
+
+                    sumOfDigits = sumOfDigits + digit % 10;
+                    digit = digit / 10;
+                }
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+
+            }
+
+        }
+    }
+}
