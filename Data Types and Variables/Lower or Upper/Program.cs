d713f149c2cb7922bea13b9fcbb397b80b909290
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-09 23:47:55 +0300

    Add files via upload

---
+++ Data Types and Variables/Lower or Upper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+            if (Char.IsUpper(symbol))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
