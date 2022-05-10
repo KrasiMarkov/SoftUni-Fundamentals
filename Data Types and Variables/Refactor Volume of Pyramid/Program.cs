66aa7dd66fb1c9d61c53c4fe335c3567ae4e6ded
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-10 23:03:55 +0300

    Add files via upload

---
+++ Data Types and Variables/Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double volume = 0;
+            volume = (length * width * height) / 3;
+            
+            Console.WriteLine($"Length: Width: Height: Pyramid Volume: {volume:F2}");
+        }
+    }
+}
