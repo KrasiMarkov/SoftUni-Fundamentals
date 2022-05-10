b11ab594210ce3e3227697f68ef150b4438bbbe7
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-10 23:04:33 +0300

    Add files via upload

---
+++ Data Types and Variables/Reversed Chars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Reversed_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sumOfSymbols = "";
+
+            for (int i = 1; i <= 3; i++)
+            {
+                char symbols = char.Parse(Console.ReadLine());
+                sumOfSymbols = sumOfSymbols + symbols;
+            }
+            
+            for (int i = sumOfSymbols.Length - 1; i >= 0; i--)
+            {
+
+                char reversedChar = sumOfSymbols[i];
+
+                Console.Write($"{reversedChar} ");
+            }
+            
+        }
+    }
+}
