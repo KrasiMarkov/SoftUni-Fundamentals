a43d428b82f48d55e83af4b032d23e5bebca3477
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-11 22:15:08 +0300

    Add files via upload

---
+++ Data Types and Variables/Special Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDigits = 0;
+                int digits = i;
+                while (digits > 0)
+                {
+                    sumOfDigits = sumOfDigits + digits % 10;
+                    digits = digits / 10;
+                }
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
