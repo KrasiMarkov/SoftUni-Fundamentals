e47f040295ad35d96f3eaa262dc11eba9144b45f
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-12 23:44:38 +0300

    Add files via upload

---
+++ Exercise - Data Types and Variables/Beer Kegs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            double maxVolume = double.MinValue;
+            double volume = double.MinValue;
+            string maxKeg = "";
+            for (int i = 1; i <= n; i++)
+            {
+                string nameKeg = Console.ReadLine();
+                float radiusKeg = float.Parse(Console.ReadLine());
+                int heightKeg = int.Parse(Console.ReadLine());
+
+                volume = Math.PI * Math.Pow(radiusKeg, 2) * heightKeg;
+
+                if (volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    maxKeg = nameKeg;
+                }
+
+            }
+            Console.WriteLine($"{maxKeg}");
+        }
+    }
+}
