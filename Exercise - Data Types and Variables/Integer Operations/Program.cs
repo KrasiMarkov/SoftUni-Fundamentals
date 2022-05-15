9f742f0dfc37800b258b35ed53c58a4dea05a8a0
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-15 23:46:04 +0300

    Add files via upload

---
+++ Exercise - Data Types and Variables/Integer Operations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberTree = int.Parse(Console.ReadLine());
+            int numberFour = int.Parse(Console.ReadLine());
+
+            int resultOne = numberOne + numberTwo;
+            int resultTwo = resultOne / numberTree;
+            int resultTree = resultTwo * numberFour;
+            Console.WriteLine(resultTree);
+        }
+    }
+}
