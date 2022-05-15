7ea071ffbf0ad94ffbb6585e0d76e26646cd6d04
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-15 23:46:38 +0300

    Add files via upload

---
+++ Exercise - Data Types and Variables/Pokemon/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            double tempPower = power * 0.5;
+            int counter = 0;
+            while (power >= distance)
+            {
+
+                counter++;
+                power = power - distance;
+                if (power == tempPower && exhaustionFactor != 0)
+                {
+                    power = power / exhaustionFactor;
+
+
+
+                }
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(counter);
+
+        }
+    }
+}
