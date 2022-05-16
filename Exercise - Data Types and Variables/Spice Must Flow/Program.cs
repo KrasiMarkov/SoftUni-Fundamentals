a0b793cfc019393bd7570264a906eb50301ee925
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-16 23:50:11 +0300

    Add files via upload

---
+++ Exercise - Data Types and Variables/Spice Must Flow/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield= int.Parse(Console.ReadLine());
+            int amount = 0;
+            int counter = 0;
+            while (startingYield >= 100)
+            {
+                amount = amount + startingYield;
+                startingYield = startingYield - 10;
+                counter++;
+                if (amount - 26 < 0)
+                {
+                    amount = 0;
+                }
+                else
+                {
+                    amount = amount - 26;
+                }
+
+            }
+            if (amount - 26 < 0)
+            {
+                amount = 0;
+            }
+            else
+            {
+                amount = amount - 26;
+            }
+            Console.WriteLine(counter);
+            Console.WriteLine(amount);
+
+        }
+    }
+}
