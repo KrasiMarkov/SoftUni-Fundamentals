b57ad1909d822754614a3ff46717cb1a4a0dfe33
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-16 23:51:19 +0300

    Add files via upload

---
+++ Exercise - Data Types and Variables/Sum of Chars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                char symbols = char.Parse(Console.ReadLine());
+                int intSymbols = symbols;
+                sum = sum + intSymbols;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
