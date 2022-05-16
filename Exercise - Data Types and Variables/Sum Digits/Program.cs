84e4b4d5d762977471c080849d05535ffcd942da
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-16 23:50:43 +0300

    Add files via upload

---
+++ Exercise - Data Types and Variables/Sum Digits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                char symbol = number[i];
+                int digit = int.Parse(symbol.ToString());
+                sum = sum + digit;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
