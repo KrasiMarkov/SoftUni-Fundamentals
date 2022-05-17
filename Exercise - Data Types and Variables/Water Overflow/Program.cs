9a7b674c3d901923378dcb46db1acbaf9a321640
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-17 23:44:17 +0300

    Add files via upload

---
+++ Exercise - Data Types and Variables/Water Overflow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 255;
+            int littersInTank = 0;
+            for (int i = 1; i <= n; i++)
+            {
+
+                int litters = int.Parse(Console.ReadLine());
+                if (litters <= sum)
+                {
+                    sum = sum - litters;
+                    littersInTank = littersInTank + litters;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+
+                }
+
+
+
+            }
+            Console.WriteLine(littersInTank);
+        }
+    }
+}
