5d80664d506837185f05110859487c26174dddef
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-18 23:26:31 +0300

    Add files via upload

---
+++ Arrays Lab/Day of Week/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            if (day < 1 || day > 7)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[day - 1]);
+            }
+
+
+        }
+    }
+}
