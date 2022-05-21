1dd61906ff4c921de46f758d9895da119787f524
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-21 22:42:52 +0300

    Add files via upload

---
+++ Arrays Lab/Equal Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+namespace Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstMassive = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondMassive = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            int index = 0;
+            bool flag = false;
+            for (int i = 0; i < firstMassive.Length; i++)
+            {
+                if (firstMassive[i] == secondMassive[i])
+                {
+                    int number = firstMassive[i];
+                    sum += number;
+                }
+                else
+                {
+                    index = i;
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == true)
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {index} index");
+            }
+            else
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
