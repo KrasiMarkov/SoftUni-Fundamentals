ca93c2678700b3efe5f51ff666b19e894aff52d6
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-21 22:43:20 +0300

    Add files via upload

---
+++ Arrays Lab/Even and Odd Subtraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumEvenNumbers = 0;
+            int sumOddNumbers = 0;
+            int result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int cell = numbers[i];
+                if (cell % 2 == 0)
+                {
+                    sumEvenNumbers += cell;
+                }
+                else
+                {
+                    sumOddNumbers += cell;
+
+                }
+
+            }
+            result = sumEvenNumbers - sumOddNumbers;
+            Console.WriteLine($"{result}");
+        }
+    }
+}
