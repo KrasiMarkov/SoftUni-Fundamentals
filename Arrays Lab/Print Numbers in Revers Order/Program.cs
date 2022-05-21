feb29a00088ddbe64a8874a5f37d726e508eeded
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-21 22:44:40 +0300

    Add files via upload

---
+++ Arrays Lab/Print Numbers in Revers Order/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Print_Numbers_in_Revers_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                int number = int.Parse(Console.ReadLine());
+                numbers[i] = number;
+            }
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
