4221721f6dd043597c2c06464912eea65bc7f26a
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-23 23:08:36 +0300

    Add files via upload

---
+++ Arrays Lab/Sum Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+                int otherNumber = numbers[i];
+                if (otherNumber % 2 == 0)
+                {
+                    sum += otherNumber;
+                }
+            }
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
