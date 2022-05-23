a21bcb0315bb32c45348c64f2f8f9f7a74abbc42
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-23 23:07:49 +0300

    Add files via upload

---
+++ Arrays Lab/Rounding Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string row = Console.ReadLine();
+            string[] numbersAsText = row.Split();
+            double[] numbers = new double[numbersAsText.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = double.Parse(numbersAsText[i]);
+                Console.WriteLine($"{numbers[i]} => {Math.Round(numbers[i], MidpointRounding.AwayFromZero)}");
+
+            }
+
+
+
+
+
+        }
+    }
+}
