e359d3beb3dca386f68256a7720cfd1faeadc6d2
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-23 23:07:19 +0300

    Add files via upload

---
+++ Arrays Lab/Reverse Array of Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string row = Console.ReadLine();
+            string[] names = row.Split();
+            for (int i = names.Length - 1; i >= 0; i--)
+            {
+                string reverseNames = names[i];
+                Console.Write($"{reverseNames} ");
+            }
+        }
+    }
+}
