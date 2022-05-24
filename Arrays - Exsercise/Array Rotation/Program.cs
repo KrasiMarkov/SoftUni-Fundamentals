737ff5fdae7fb7ceca82417db9800da941fab28d
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-24 23:41:23 +0300

    Add files via upload

---
+++ Arrays - Exsercise/Array Rotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= rotations; i++)
+            {
+                string temp = numbers[0];
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    numbers[j] = numbers[j + 1];
+                }
+
+                numbers[numbers.Length - 1] = temp;
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
