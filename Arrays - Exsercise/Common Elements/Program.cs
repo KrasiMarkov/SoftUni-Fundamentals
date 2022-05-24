78f03fa9b8d9f3f3de0cfc89d702a5bf392f800a
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-24 23:41:58 +0300

    Add files via upload

---
+++ Arrays - Exsercise/Common Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split().ToArray();
+            string[] secondArray = Console.ReadLine().Split().ToArray();
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                for (int j = 0; j < firstArray.Length; j++)
+                {
+                    if (secondArray[i] == firstArray[j])
+                    {
+                        Console.Write($"{secondArray[i]} ");
+                    }
+                }
+            }
+        }
+    }
+}
