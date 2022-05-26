6942217e9aeae929d555e7e7aa4fa5258e714a15
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-26 23:30:46 +0300

    Add files via upload

---
+++ Arrays - Exsercise/Magic Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+namespace Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int current = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int sum = current + arr[j];
+                    if (sum == number)
+                    {
+                        Console.Write($"{current} {arr[j]}");
+                        Console.WriteLine();
+                        
+                        
+                        break;
+                    }
+                    
+                }
+
+
+            }
+        }
+    }
+}
