05ec210306b4b3de1a01413d7606b270f8eff9de
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-28 16:58:12 +0300

    Add files via upload

---
+++ Arrays - Exsercise/Top integer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+namespace Top_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string result = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int current = numbers[i];
+                bool isTopInteger = true;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (current <= numbers[j])
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    result += current + " ";
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
