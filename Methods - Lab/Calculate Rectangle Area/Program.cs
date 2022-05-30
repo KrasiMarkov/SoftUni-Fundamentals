18915e137925518720102433810c15352fab60de
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-30 23:29:09 +0300

    Add files via upload

---
+++ Methods - Lab/Calculate Rectangle Area/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int result = CalculateArea(width, height);
+            Console.WriteLine(result);
+        }
+
+        static int CalculateArea(int width, int height)
+        {
+             
+            return width * height;
+
+
+        }
+    }
+}
