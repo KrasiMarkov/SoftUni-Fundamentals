9e64f2d2e8e39066e42e5d386a5fc1d5f4cab531
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-31 23:22:37 +0300

    Add files via upload

---
+++ Methods - Lab/Math operations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNUmber = double.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            if (symbol == '*')
+            {
+
+                Console.WriteLine(MultiplyNumbers(firstNUmber, secondNumber));
+            }
+            else if (symbol == '/')
+            {
+                Console.WriteLine(DivideNumbers(firstNUmber, secondNumber));
+            }
+            else if (symbol == '+')
+            {
+                Console.WriteLine(SumNumbers(firstNUmber, secondNumber));
+            }
+            else if (symbol == '-')
+            {
+                Console.WriteLine(SubtractNumbers(firstNUmber, secondNumber));
+            }
+        }
+
+        static double MultiplyNumbers(double firstNumber, double secondNumber)
+        {
+            double rezult = firstNumber * secondNumber;
+            return rezult;
+        }
+
+        static double DivideNumbers(double firstNumber, double secondNumber)
+        {
+            double rezult = firstNumber / secondNumber;
+            return rezult;
+        }
+
+        static double SumNumbers(double firstNumber, double secondNumber)
+        {
+            double rezult = firstNumber + secondNumber;
+            return rezult;
+        }
+
+        static double SubtractNumbers(double firstNumber, double secondNumber)
+        {
+            double rezult = firstNumber - secondNumber;
+            return rezult;
+        }
+    }
+}
