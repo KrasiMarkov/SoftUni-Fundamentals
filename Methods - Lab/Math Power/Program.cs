fc7e52ced620752a418da53ad71ffcc57b9e2ed6
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-01 23:30:28 +0300

    Add files via upload

---
+++ Methods - Lab/Math Power/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            int pow = int.Parse(Console.ReadLine());
+            double powered = CalculatePower(n, pow);
+            Console.WriteLine(powered);
+
+        }
+
+        static double CalculatePower(double n, int pow)
+        {
+            double result = 1.0d;
+
+            for (int i = 1; i <= pow; i++)
+            {
+                result *= n;
+            }
+            return result;
+
+
+        }
+    }
+}
