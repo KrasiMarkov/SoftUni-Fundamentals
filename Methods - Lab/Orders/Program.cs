3d9cfcfae74cbe568e4c7c42dba0bd23bbc794fe
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-01 23:31:32 +0300

    Add files via upload

---
+++ Methods - Lab/Orders/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            TotalPrice(product, quantity);
+
+        }
+        static void TotalPrice(string product, int quantity)
+        {
+            switch (product)
+            {
+                case "coffee":
+                    Console.WriteLine($"{(quantity * 1.50):F2}");
+                    break;
+                case "water":
+                    Console.WriteLine($"{(quantity * 1.00):F2}");
+                    break;
+                case "coke":
+                    Console.WriteLine($"{(quantity * 1.40):F2}");
+                    break;
+                case "snacks":
+                    Console.WriteLine($"{(quantity * 2.00):F2}");
+                    break;
+                default:
+                    break;
+            }
+           
+        }
+
+
+    }
+}
