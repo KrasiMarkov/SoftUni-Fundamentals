c8ca6fcd62485796a4977052d8c6543b7da83d38
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-02 23:37:52 +0300

    Add files via upload

---
+++ Methods - Lab/Repeat String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+            string newName = ReturnNString(name, number);
+            Console.WriteLine(newName);
+            
+        }
+
+        static string ReturnNString(string name, int number)
+        {
+            string result = "";
+            for (int i = 1; i <= number; i++)
+            {
+
+                result += name;
+            }
+            return result;
+        }
+    }
+}
