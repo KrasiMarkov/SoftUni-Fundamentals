d213efdab17c2abc7f3d5ebc75c035fdc37a58a8
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-02 23:38:20 +0300

    Add files via upload

---
+++ Methods - Lab/Sign of Integers Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sign_of_Integers_Numbers
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            PrintSign(int.Parse(Console.ReadLine()));
+        }
+
+        static void PrintSign( int n)
+        {
+            
+            if (n > 0)  
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+        }
+    }
+}
