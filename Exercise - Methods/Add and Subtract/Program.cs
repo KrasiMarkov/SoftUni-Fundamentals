71bd7a834100cf329622c98711946551a0a90f2a
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-07 23:34:45 +0300

    Add files via upload

---
+++ Exercise - Methods/Add and Subtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(Subtract(firstNumber, secondNumber, thirdNumber));
+        }
+
+        static int Sum(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            return sum;
+            
+        }
+
+        static int Subtract(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int result = Sum(firstNumber, secondNumber, thirdNumber) - thirdNumber;
+            return result;
+        }
+    }
+}
