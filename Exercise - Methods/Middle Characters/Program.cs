dcc5fad5e70efb6ddb30e49255f35aa05001e53e
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-08 23:23:17 +0300

    Add files via upload

---
+++ Exercise - Methods/Middle Characters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            PrintMiddleCharacter(text);
+        }
+
+        static void PrintMiddleCharacter(string text)
+        {
+            if (text.Length % 2 == 0)
+            {
+                char firstSymbol = text[text.Length / 2 - 1];
+                char secondSymbol = text[text.Length / 2];
+                Console.WriteLine($"{firstSymbol}{secondSymbol}");
+            }
+            else
+            {
+                char symbol = text[text.Length / 2];
+                Console.WriteLine($"{symbol}");
+            }
+        }
+    }
+}
