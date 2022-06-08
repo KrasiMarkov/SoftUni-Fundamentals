695c62ea3f367dcd0917db4e675f00e99dfbf613
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-08 23:22:43 +0300

    Add files via upload

---
+++ Exercise - Methods/Factorial Division/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            double firstFactorial = GetFirstFactorial(firstNumber);
+            double secondFactorial = GetSecondFactorial(secondNumber);
+
+            double result = firstFactorial / secondFactorial;
+            Console.WriteLine($"{result:F2}");
+        }
+
+
+
+
+        static double GetFirstFactorial(int firstNumber)
+        {
+
+            double firstFactorial = firstNumber;
+
+            for (int i = firstNumber - 1; i >= 1; i--)
+            {
+                firstFactorial = firstFactorial * i;
+            }
+            return firstFactorial;
+
+
+
+
+
+
+
+
+        }
+
+        static double GetSecondFactorial(int secondNumber)
+        {
+            double secondFactorial = secondNumber;
+
+            for (int i = secondNumber - 1; i >= 1; i--)
+            {
+                secondFactorial = secondFactorial * i;
+            }
+            return secondFactorial;
+        }
+
+    }   
+    
+}
