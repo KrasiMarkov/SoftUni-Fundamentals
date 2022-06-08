8cc85cdf7b0f5cfd4687d98ee7a710f39be5ca01
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-08 23:22:09 +0300

    Add files via upload

---
+++ Exercise - Methods/Characters in Range/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            
+            SymbolsFromASCII(firstSymbol, secondSymbol);
+
+
+        }
+
+        static void SymbolsFromASCII(char firstSymbol, char secondSymbol)
+        {
+            int asciiFirstSymbol = Math.Min(firstSymbol, secondSymbol);
+            int asciiSecondSymbol = Math.Max(firstSymbol, secondSymbol);
+
+            for (int i = asciiFirstSymbol + 1; i < asciiSecondSymbol; i++)
+            {
+                char newSymbol = (char)i;
+                Console.Write($"{newSymbol} ");
+            }
+            
+        }
+    }
+}
