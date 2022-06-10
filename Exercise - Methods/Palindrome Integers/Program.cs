ed359043e2e1e4f47e0cf22d3ce422bced872538
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-10 23:36:45 +0300

    Add files via upload

---
+++ Exercise - Methods/Palindrome Integers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            PrintBool(command);
+            
+
+        }
+
+        static void PrintBool(string command)
+        {
+            while (command != "END")
+            {
+                string reverseString = "";
+                for (int i = command.Length - 1; i >= 0; i--)
+                {
+                    char symbol = command[i];
+                    reverseString = reverseString + symbol;
+                }
+                if (reverseString == command)
+                {
+                    Console.WriteLine("true");
+
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+
+                command = Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
