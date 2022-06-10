85d655dbda9465a68036488ca1d96b32e09618fe
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-10 23:36:08 +0300

    Add files via upload

---
+++ Exercise - Methods/NxN Matrix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintNxN(n);
+            
+            
+        }
+        static void PrintNxN(int n)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n; j > 0; j--)
+                {
+                    Console.Write($"{n} ");
+
+
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+        }
+    }
+}
