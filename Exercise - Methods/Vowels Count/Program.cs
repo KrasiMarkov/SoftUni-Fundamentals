98277da2f18e59e428d5fde575d300a49f4ff22d
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-12 23:54:27 +0300

    Add files via upload

---
+++ Exercise - Methods/Vowels Count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine().ToLower();
+            Console.WriteLine(ReturnCountVowels(name));
+        }
+
+        static int ReturnCountVowels(string name)
+        {
+            int sum = 0;
+            for (int i = 0; i < name.Length; i++)
+            {
+                char symbol = name[i];
+                if (symbol == 'e' || symbol == 'a' || symbol == 'i' || symbol == 'u' || symbol == 'o')
+                {
+                    sum++;
+                }
+
+            }
+            return sum;
+        }
+    }
+}
