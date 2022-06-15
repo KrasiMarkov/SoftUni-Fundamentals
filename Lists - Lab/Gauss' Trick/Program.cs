1b280c5f03b2b8e59fa9f60f2a86c37600162def
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-15 23:41:36 +0300

    Add files via upload

---
+++ Lists - Lab/Gauss' Trick/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int size = numbers.Count / 2;
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+               
+            }
+           
+            Console.WriteLine(string.Join(" ", numbers));
+
+
+        }
+    }
+}
