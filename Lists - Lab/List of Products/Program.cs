7a5db466d8653146a3c3fd6072fc7140437d8951
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-16 23:36:18 +0300

    Add files via upload

---
+++ Lists - Lab/List of Products/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> nameOfProducts = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine();
+                nameOfProducts.Add(product);
+
+            }
+            nameOfProducts.Sort();
+            for (int i = 0; i < nameOfProducts.Count; i++)
+            {
+                
+                Console.WriteLine($"{i + 1}.{nameOfProducts[i]}");
+            }
+        }
+    }
+}
