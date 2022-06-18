5e319c971fc0692d57702f8a9b6e229501a6c035
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-18 23:12:20 +0300

    Add files via upload

---
+++ Lists - Lab/Merging Lists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstNumber = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondNumber = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> thirdNumber = new List<int>();
+
+            for (int i = 0; i < Math.Max(firstNumber.Count, secondNumber.Count); i++)
+            {
+                if (i < firstNumber.Count)
+                {
+                    thirdNumber.Add(firstNumber[i]);
+                }
+                if (i < secondNumber.Count)
+                {
+                    thirdNumber.Add(secondNumber[i]);
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", thirdNumber));
+
+        }
+    }
+}
