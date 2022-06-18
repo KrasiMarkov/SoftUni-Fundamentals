0cf48f59529d90998cb3e3acc1b74b89d67e9492
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-18 23:12:55 +0300

    Add files via upload

---
+++ Lists - Lab/Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int oneNumber = numbers[i];
+                if (oneNumber < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i = -1;
+                }
+
+
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                numbers.Reverse();
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
