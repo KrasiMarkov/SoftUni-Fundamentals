46e0bca543e8a02b75146034d71c32af414788a5
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-20 23:17:27 +0300

    Add files via upload

---
+++ List - Exercise/Append Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbersAsString = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+
+            List<string> reversedNumbers = new List<string>();
+
+            for (int i = numbersAsString.Length-1; i >= 0; i--)
+            {
+                string[] finalArray = numbersAsString[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                
+                reversedNumbers.AddRange(finalArray);
+
+
+            }
+
+            Console.WriteLine(string.Join(" ", reversedNumbers));
+
+
+
+
+
+        }
+    }
+}
