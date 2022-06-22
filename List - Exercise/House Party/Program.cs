4a1d7ca1643a4db40649de3a93fb3998d3d8f702
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-22 23:49:09 +0300

    Add files via upload

---
+++ List - Exercise/House Party/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace House_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+
+                string[] othersCommand = command.Split();
+
+                string name = othersCommand[0];
+
+                if (othersCommand[1] == "is" && othersCommand[2] == "going!")
+                {
+                    bool check = names.Contains(name);
+
+                    if (check == false)
+                    {
+                        names.Add(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+
+                }
+                else if(othersCommand[2] == "not")
+                {
+                    bool check = names.Contains(name);
+
+                    if (check == true)
+                    {
+                        names.Remove(name);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+
+
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
