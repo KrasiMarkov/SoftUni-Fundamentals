d43a58ec8a0d5d3b5765838b77f31a604c27dc16
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-23 23:52:57 +0300

    Add files via upload

---
+++ List - Exercise/Train/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int maxCapacityOfWagon = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+
+
+                string command = Console.ReadLine();
+
+                string[] otherCommand = command.Split();
+
+
+
+                if (otherCommand[0] == "end")
+                {
+                    break;
+                }
+
+
+
+                if (otherCommand[0] == "Add")
+                {
+                    int passengers = int.Parse(otherCommand[1]);
+                    numbers.Add(passengers);
+                }
+                else
+                {
+                    int numPassengers = int.Parse(otherCommand[0]);
+
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        int currentPassengers = numbers[i];
+
+                        int sum = numPassengers + currentPassengers;
+
+                        if (sum <= maxCapacityOfWagon)
+                        {
+                            numbers[i] = sum;
+                            break;
+                        }
+                    }
+                }
+
+
+
+
+
+
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
