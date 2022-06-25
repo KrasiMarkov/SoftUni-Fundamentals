a5efc4a15df9b6f6dc49930f58fd75e1ddb2a469
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-25 23:28:57 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/Big Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+namespace Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+
+
+            BigInteger factorial = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine(factorial);
+
+        }
+    }
+}
