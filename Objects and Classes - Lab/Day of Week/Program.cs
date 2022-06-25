9a0ae76356f55b6c69a4f72465258ded683b3c35
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-25 23:29:49 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/Day of Week/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+namespace Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
