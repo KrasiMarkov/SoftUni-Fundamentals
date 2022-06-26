fff3c0f568b96884d55d5e96bb3cc9c929685227
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-26 23:15:07 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/Randomize Words/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+namespace Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split().ToList();
+
+            var random = new Random();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var randomIndex = random.Next(0, list.Count);
+
+                var randomWord = list[randomIndex];
+
+                var word = list[i];
+
+                list[randomIndex] = word;
+
+                list[i] = randomWord;
+
+            }
+            foreach (var words in list)
+            {
+                Console.WriteLine(words);
+            }
+        }
+    }
+}
