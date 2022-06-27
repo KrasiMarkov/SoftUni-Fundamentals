bc7ea808bbffb5e7ad7575e425da6e84e1abd975
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-06-27 23:50:11 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/Store Boxes/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store_Boxes
+{
+    class Box
+    {
+        public string SerialNumber { get; set; }
+
+        public Item Item { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal priceForABox => Item.Price * ItemQuantity;
+
+    }
+}
---
+++ Objects and Classes - Lab/Store Boxes/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+namespace Store_Boxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command[0] == "end")
+                {
+                    break;
+                }
+
+                string serialNumber = command[0];
+
+                string itemName = command[1];
+
+                int itemQuantity = int.Parse(command[2]);
+
+                decimal itemPrice = decimal.Parse(command[3]);
+
+                Item item = new Item();
+                item.Name = itemName;
+                item.Price = itemPrice;
+
+                Box box = new Box();
+                box.SerialNumber = serialNumber;
+                box.Item = item;
+                box.ItemQuantity = itemQuantity;
+
+
+
+
+
+                boxes.Add(box);
+
+            }
+
+            foreach (var currentBox in boxes.OrderByDescending(x => x.priceForABox))
+            {
+                Console.WriteLine($"{currentBox.SerialNumber}");
+                Console.WriteLine($"-- {currentBox.Item.Name} - ${currentBox.Item.Price:F2}: {currentBox.ItemQuantity}");
+                Console.WriteLine($"-- ${currentBox.priceForABox:F2}");
+            }
+        }
+    }
+    class Item
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    class Box
+    {
+        public string SerialNumber { get; set; }
+
+        public Item Item { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal priceForABox => Item.Price * ItemQuantity;
+
+    }
+}
