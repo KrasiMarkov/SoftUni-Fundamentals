df978b9ab82bfc27938e2d8c12bf4c662298862c
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-08 23:58:51 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/Count Real Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!count.ContainsKey(number))
+                {
+                    count[number] = 1;
+                }
+                else
+                {
+                    count[number]++;
+                }
+
+
+            }
+
+            foreach (var item in count)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
