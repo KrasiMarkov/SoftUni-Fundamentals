a32ce7bc92910710f3c340ae8b9b441c2d7e3c76
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-08 23:59:48 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/Largest 3 Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(n => n).ToList();
+
+            if (numbers.Count >= 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+
+        }
+    }
+}
