5524dc4f390dc592711884d4f24ca1427bc5409b
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-11 23:42:52 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/Odd Occurrences/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split().ToList();
+
+            Dictionary<string, int> output = new Dictionary<string, int>();
+
+            List<string> total = new List<string>();
+
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                string name = input[i];
+                input[i] = name.ToLower();
+                
+            }
+              
+            
+
+
+            foreach (var item in input)
+            {
+                if (!output.ContainsKey(item))
+                {
+                    output[item] = 1;
+
+                }
+                else
+                {
+                    output[item]++;
+                }
+            }
+
+            foreach (var item in output)
+            {
+                if (item.Value % 2 == 1)
+                {
+                    total.Add(item.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", total));
+        }
+    }
+}
