3a73f07d3c5e43b1dc606dfa4a67bdb0012db206
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-18 23:06:16 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/Word Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+
+            string[] filtredData = words.Where(x => x.Length % 2 == 0).ToArray();
+
+            foreach (var item in filtredData)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
