378aadb428cd2e3dacbc1f0473d96a6bf37f1f1f
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-19 23:20:40 +0300

    Add files via upload

---
+++ Associative Arrays - Exercise/A Miner Task/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Dictionary<string, int> resource = new Dictionary<string, int>();
+
+
+            while (command != "stop")
+            {
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resource.ContainsKey(command))
+                {
+                    resource[command] = quantity;
+                }
+
+                else
+                {
+                    resource[command] += quantity;
+                }
+                
+
+
+
+                command = Console.ReadLine();
+
+
+            }
+
+            foreach (var item in resource)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
