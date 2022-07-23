811b599b6a5d60b57bd31aed9df4a12057360352
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-23 23:52:55 +0300

    Add files via upload

---
+++ Associative Arrays - Exercise/Student Academy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> averageGrade = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!averageGrade.ContainsKey(name))
+                {
+                    averageGrade[name] = new List<double>();
+
+                    averageGrade[name].Add(grade);
+                }
+
+                else
+                {
+                    averageGrade[name].Add(grade);
+                }
+
+
+            }
+
+            
+
+            foreach (var item in averageGrade.OrderByDescending(x => x.Value.Average()))
+            {
+                if (item.Value.Average() >= 4.5)
+                {
+                    Console.WriteLine($"{item.Key} -> {item.Value.Average():F2}");
+                }
+            }
+        }
+    }
+}
