ec962cccf8f44704a75e825f3fa622f69297b7fb
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-24 23:33:51 +0300

    Add files via upload

---
+++ Text Processing - Lab/Digits, Letters and Other/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            string digits = "";
+
+            string letters = "";
+
+            string othersSymbols = "";
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char symbol = word[i];
+
+                if (char.IsDigit(symbol))
+                {
+                    digits += symbol;
+                }
+
+                else if (char.IsLetter(symbol))
+                {
+                    letters += symbol;
+                }
+
+                else if (char.IsSymbol(symbol))
+                {
+                    othersSymbols += symbol;
+                }
+
+                else if (char.IsPunctuation(symbol))
+                {
+                    othersSymbols += symbol;
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(othersSymbols);
+
+        }
+    }
+}
