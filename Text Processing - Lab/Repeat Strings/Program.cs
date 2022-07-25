8ffad4b50e1c6650f8d76b7769240b270c78830a
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-25 22:30:28 +0300

    Add files via upload

---
+++ Text Processing - Lab/Repeat Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+
+            string finalResult = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string symbols = words[i];
+
+                int result = symbols.Length;
+
+                for (int j = 0; j < result; j++)
+                {
+                    finalResult += symbols;
+                }
+            }
+
+            Console.WriteLine(finalResult);
+        }
+    }
+}
