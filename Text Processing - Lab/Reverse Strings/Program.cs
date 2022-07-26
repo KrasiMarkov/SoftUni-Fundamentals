a8ca2b8369feb15d72ffbdcfdb837ad9b57cd81e
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-26 23:20:05 +0300

    Add files via upload

---
+++ Text Processing - Lab/Reverse Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string result = "";
+
+                string word = Console.ReadLine();
+
+                if (word == "end")
+                {
+                    break;
+                }
+
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    char symbol = word[i];
+
+                    result = result + symbol;
+                }
+
+                Console.WriteLine($"{word} = {result}");
+
+            }
+        }
+    }
+}
