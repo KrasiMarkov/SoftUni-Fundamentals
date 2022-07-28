03d03b1606db0795e800188a426d1ec2909d6f18
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-28 23:13:44 +0300

    Add files via upload

---
+++ Text Processing - Lab/Text Filter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(", ").ToArray();
+
+            string text = Console.ReadLine();
+
+            foreach (var item in words)
+            {
+                if (text.Contains(item))
+                {
+                    text = text.Replace(item, new string('*', item.Length));
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
