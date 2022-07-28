53ee6ebda428ea7ebe3dd204ce3f2f7cfc82c98e
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-28 23:13:12 +0300

    Add files via upload

---
+++ Text Processing - Lab/Substring/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string word = Console.ReadLine().ToLower();
+
+            string text = Console.ReadLine();
+
+            int result = -1;
+
+           
+
+            while (true)
+            {
+                result = text.IndexOf(word);
+
+              
+
+                if (result == -1)
+                {
+                    break; 
+                }
+
+
+                text = text.Remove(result, word.Length);
+            }
+
+            Console.WriteLine(text);
+
+        }
+    }
+}
