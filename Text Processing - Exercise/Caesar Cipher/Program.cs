ae0ef48d6b172939ba56f6851ac76d3d38936a96
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-07-31 23:45:27 +0300

    Add files via upload

---
+++ Text Processing - Exercise/Caesar Cipher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string finalStr = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int symbol = text[i];
+
+                symbol += 3;
+
+                char newSymbol = (char)symbol;
+
+                finalStr += newSymbol;
+
+            }
+
+            Console.WriteLine(finalStr);
+        }
+    }
+}
