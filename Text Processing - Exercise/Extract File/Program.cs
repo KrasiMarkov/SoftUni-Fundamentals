1bfe6c0ecd0832dd913a4d12d24400da1147b0ac
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-02 23:06:05 +0300

    Add files via upload

---
+++ Text Processing - Exercise/Extract File/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> text = Console.ReadLine().Split('\\').ToList();
+
+            List<string> words = new List<string>();
+
+            string total = text[text.Count - 1];
+
+            string[] final = total.Split('.');
+
+            Console.WriteLine($"File name: {final[0]}");
+            Console.WriteLine($"File extension: {final[1]}");
+
+
+
+
+
+
+
+
+        }
+    }
+}
