5fbd1999f7efa7be69f3dedb4d02444bec28e382
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-04 23:42:19 +0300

    Add files via upload

---
+++ Text Processing - Exercise/Replace Repeating Chars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequence = Console.ReadLine();
+
+            string result = "";
+
+            char lastSymbol = '!';
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                char symbol = sequence[i];
+
+                if (symbol == lastSymbol)
+                {
+                    continue;
+                }
+
+                result += symbol;
+
+                lastSymbol = symbol;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
