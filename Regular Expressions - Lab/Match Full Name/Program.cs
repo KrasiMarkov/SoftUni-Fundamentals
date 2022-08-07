f75789fb8052ae0f1235d6952a8158ceba959394
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-07 16:03:09 +0300

    Add files via upload

---
+++ Regular Expressions - Lab/Match Full Name/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+namespace Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+
+            string text = Console.ReadLine();
+
+            var matches = regex.Matches(text);
+
+            var names = "";
+
+            foreach (Match match in matches)
+            {
+
+                names = names + match + " ";
+                
+            }
+
+            Console.WriteLine(names);
+        }
+    }
+}
