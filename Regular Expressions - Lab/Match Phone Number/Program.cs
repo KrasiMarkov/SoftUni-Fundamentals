40d8303c5c41cdccc0ed993c1ad0d5f26c424fa2
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-08 23:42:30 +0300

    Add files via upload

---
+++ Regular Expressions - Lab/Match Phone Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+namespace Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine();
+
+            var regex = new Regex(@"\+359(-| )2\1[0-9]{3}\1[0-9]{4}\b");
+
+            var matches = regex.Matches(numbers);
+
+
+
+            Console.WriteLine(string.Join(", ", matches));
+            
+        } 
+    }
+}
