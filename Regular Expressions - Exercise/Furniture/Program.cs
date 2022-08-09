49e0cc5a6b46ebbb68df0fa655d8cbeedb7fd319
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-09 22:26:01 +0300

    Add files via upload

---
+++ Regular Expressions - Exercise/Furniture/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> total = new List<string>();
+
+            decimal totalMoney = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+
+                if (command == "Purchase")
+                {
+                    break;
+                }
+
+                var regex = new Regex(@">>(?<name>.+)<<(?<price>[0-9]+\.?[0-9]*)!(?<quantity>[0-9]+)");
+
+                var match = regex.Match(command);
+
+                if (match.Success == true)
+                {
+
+                    var name = match.Groups["name"].Value;
+
+                    var price = match.Groups["price"].Value;
+
+                    var quantity = match.Groups["quantity"].Value;
+
+                    decimal priceToNumber = decimal.Parse(price);
+
+                    int quantityToNumber = int.Parse(quantity);
+
+
+                    total.Add(name);
+
+                    totalMoney += priceToNumber * quantityToNumber;
+
+
+
+
+
+                }
+
+
+
+            }
+
+            Console.WriteLine("Bought furniture:");
+
+            foreach (var item in total)
+            {
+
+
+                Console.WriteLine($"{item}");
+
+            }
+
+            Console.WriteLine($"Total money spend: {totalMoney:F2}");
+        }
+    }
+}
