3eb39ee5a9e933f3d7989690a1727029435259be
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-08-11 23:58:31 +0300

    Add files via upload

---
+++ Regular Expressions - Exercise/Nether Realms/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace Nether_Realms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var healthRegex = new Regex(@"[^0-9+\-*\/.]");
+
+            var digitRegex = new Regex(@"-?[0-9]\.?[0-9]*");
+
+            var operatorRegex = new Regex(@"[*\/]");
+
+            string[] demonNames = Regex.Split(Console.ReadLine(), @"\s*,\s*").OrderBy(x => x).ToArray();
+
+            for (int i = 0; i < demonNames.Length; i++)
+            {
+                string currentDemon = demonNames[i];
+
+                int currentHealth = 0;
+
+                var healthSymbols = healthRegex.Matches(currentDemon);
+
+                foreach (Match symbol in healthSymbols)
+                {
+                    currentHealth += char.Parse(symbol.Value);
+                }
+
+                var digitMatch = digitRegex.Matches(currentDemon);
+
+                double baseDamage = 0;
+
+                foreach (Match number in digitMatch)
+                {
+                    baseDamage += double.Parse(number.Value);
+                }
+
+                var operatorMatch = operatorRegex.Matches(currentDemon);
+
+                foreach (Match operatorr in operatorMatch)
+                {
+                    string @operator = operatorr.Value;
+
+                    if (@operator == "*")
+                    {
+                        baseDamage = baseDamage * 2;
+                    }
+
+                    else
+                    {
+                        baseDamage = baseDamage / 2;
+                    }
+                }
+
+
+                Console.WriteLine($"{currentDemon} - {currentHealth} health, {baseDamage:F2} damage");
+            }
+
+
+        }
+    }
+}
